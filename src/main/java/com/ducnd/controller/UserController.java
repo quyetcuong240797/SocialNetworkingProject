02671d74fec71be829b46703900b285a618f03fd
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2018-05-13 11:43:45 +0700

    register

--- src/main/java/com/ducnd/controller/UserController.java
+++ src/main/java/com/ducnd/controller/UserController.java
@@ -1,13 +1,23 @@
 package com.ducnd.controller;
 
 import com.ducnd.Constants;
+import com.ducnd.manager.UserManager;
+import com.ducnd.model.request.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping(Constants.ENPOINT_REGISTER)
-    public String getMessgae(){
-        return "rest";
+    @Autowired
+    private UserManager userManager;
+
+    @PostMapping(Constants.ENPOINT_REGISTER)
+    public Object register(
+            @RequestBody RegisterRequest request
+            ){
+        return userManager.register(request);
     }
 }
---
+++ src/main/java/com/ducnd/model/response/response/RegisterResponse.java
@@ -0,0 +1,13 @@
+package com.ducnd.model.response.response;
+
+public class RegisterResponse {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
